b379d93249e0145b624126c8ff4afdea780e9cef
Shane <obries47@tcd.ie>
2017-10-17 19:27:32 +0100

    Working for multiple regression metrics

--- linear_regression_sum_dataset.py
+++ linear_regression_sum_dataset.py
@@ -26,42 +26,46 @@ filename2 = "sum_ds_wn.csv"
 # filename4 = "sum_ds_nn.csv"
 
 filenames = [filename1, filename2];
+# sample_sizes = [100,500,1000]
 sample_sizes = [100,500,1000,5000,10000,50000,100000,500000,1000000,5000000,10000000,50000000,100000000]
 allResults = []
 results = []
+
+regression_metrics = ['neg_mean_squared_error','r2']
+
 # create a python list of feature names
 feature_cols = ['Feature 1', 'Feature 2', 'Feature 3', 'Feature 4', 'Feature 6', 'Feature 7', 'Feature 8', 'Feature 9', 'Feature 10']
 
 for filename in filenames:
-    results = [filename]
+    for regression_metric in regression_metrics:
+        results = [filename+regression_metric]
+        for sample_size in sample_sizes:
+            # read CSV file directly from path and save the results
+            data = pd.read_csv(filename1, sep=';', index_col = 0, nrows=sample_size) # 'sep' specifies separator used in the CSV file
+            data = data.dropna()
+            # use the list to create a subset of the original DataFrame (X)
+            X = data.loc[:,feature_cols] # for 100 rows select the 'feature cols'
 
-    for sample_size in sample_sizes:
-        # read CSV file directly from path and save the results
-        data = pd.read_csv(filename1, sep=';', index_col = 0, nrows=sample_size) # 'sep' specifies separator used in the CSV file
+            # select the Target column as the response (Y)
+            y = data.Target # select first 100 elements from the Target
 
-        # use the list to create a subset of the original DataFrame (X)
-        X = data.loc[:,feature_cols] # for 100 rows select the 'feature cols'
+            # 10-fold cross validation with linear regression, using RMSE (root mean squared error) metric
 
-        # select the Target column as the response (Y)
-        y = data.Target # select first 100 elements from the Target
+            linear_reg = LinearRegression() # instantiate model
 
-        # 10-fold cross validation with linear regression, using RMSE (root mean squared error) metric
+            scores = cross_val_score(linear_reg, X, y, cv=10, scoring=regression_metric)
+            # note: cross_val_score takes care of splitting the data into folds,
+            # so we don't need to split the data ourselves using train/test split
+            if regression_metric == "neg_mean_squared_error":
+                # fix the sign of MSE scores
+                mse_scores = -scores
+                # convert from MSE to RMSE
+                scores = np.sqrt(mse_scores)
 
-        linear_reg = LinearRegression() # instantiate model
+            # calculate average RMSE
+            print ("["+ filename+": "+ regression_metric +"] Mean score for sample size " + str(sample_size) + " : " + str(scores.mean()))
+            results.append(scores.mean());
 
-        scores = cross_val_score(linear_reg, X, y, cv=10, scoring='neg_mean_squared_error')
-        # note: cross_val_score takes care of splitting the data into folds,
-        # so we don't need to split the data ourselves using train/test split
-
-        # fix the sign of MSE scores
-        mse_scores = -scores
-        # convert from MSE to RMSE
-        rmse_scores = np.sqrt(mse_scores)
-
-        # calculate average RMSE
-        print ("["+ filename +"] Mean score for sample size " + str(sample_size) + " : " + str(rmse_scores.mean()))
-        results.append(rmse_scores.mean());
-
-    allResults.append(results)
+            allResults.append(results)
 
 printResultsToCsv()
