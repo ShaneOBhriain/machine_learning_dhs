bb5a6badefe24d608402b2567847859d2026304a
Shane <obries47@tcd.ie>
2017-10-17 17:35:23 +0100

    Loops and prints for all desired sample sizes.

--- linear_regression_sum_dataset.py
+++ linear_regression_sum_dataset.py
@@ -1,35 +1,50 @@
-import numpy as np
+# -*- coding: utf-8 -*-
 import pandas as pd
-import csv, itertools
-import statsmodels.formula.api as smf
+import numpy as np
+import os
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
 
-input_file = "sum_ds_nn.csv"
+multiplier = 5
+number_of_samples = 100
 
+answers = {}
 
-df = pd.read_csv(input_file, header=0, sep=";", nrows=10)
+# specify data set path
+data_path = os.path.abspath('Data Sets/The SUM dataset/without noise/The SUM dataset, without noise.csv')
+filename = "sum_ds_nn.csv"
 
-np.polyfit(df[])
+# create a python list of feature names
+feature_cols = ['Feature 1', 'Feature 2', 'Feature 3', 'Feature 4', 'Feature 6', 'Feature 7', 'Feature 8', 'Feature 9', 'Feature 10']
 
+while number_of_samples <= 100000000:
+    # read CSV file directly from path and save the results
+    data = pd.read_csv(filename, sep=';', index_col = 0, nrows=number_of_samples) # 'sep' specifies separator used in the CSV file
 
+    # use the list to create a subset of the original DataFrame (X)
+    X = data.loc[:,feature_cols] # for 100 rows select the 'feature cols'
 
+    # select the Target column as the response (Y)
+    y = data.Target # select first 100 elements from the Target
 
-index =0
-for key in df.keys():
-    print("index " + str(index) + ": " + key)
-    index = index+1
+    # 10-fold cross validation with linear regression, using RMSE (root mean squared error) metric
 
+    linear_reg = LinearRegression() # instantiate model
 
-# df = df._get_numeric_data()
+    scores = cross_val_score(linear_reg, X, y, cv=10, scoring='neg_mean_squared_error')
+    # note: cross_val_score takes care of splitting the data into folds,
+    # so we don't need to split the data ourselves using train/test split
 
-# # put the numeric column names in a python list
-# numeric_headers = list(df.columns.values)
-#
-# # create a numpy array with the numeric values for input into scikit-learn
-# numpy_arracy = df.as_matrix()
-#
-# # reverse the order of the columns
-# numeric_headers.reverse()
-# reverse_df = df[numeric_headers]
-#
-# # write the reverse_df to an excel spreadsheet
-# reverse_df.to_csv('reversed_headers.csv')
+    # fix the sign of MSE scores
+    mse_scores = -scores
+    # convert from MSE to RMSE
+    rmse_scores = np.sqrt(mse_scores)
+
+    # calculate average RMSE
+    print ("Mean score for sample size " + str(number_of_samples) + " : " + str(rmse_scores.mean()))
+
+    number_of_samples = number_of_samples * multiplier
+    if multiplier == 5:
+        multiplier = 2
+    else:
+        multiplier = 5
