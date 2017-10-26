18df5c0c35e5d8e052f6b47e918c54ee1b75eaee
Shane <obries47@tcd.ie>
2017-10-26 13:15:23 +0100

    Better labelling in config

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 from sklearn.metrics import make_scorer
 from sklearn.metrics import precision_score
 
-result_file_name = "results.csv"
+result_file_name = "results_no_random_state.csv"
 
-sample_sizes = [100,500,1000,5000,10000,50000]
-
+sample_sizes = [100,500,1000]
 sum_features = ['Feature 1', 'Feature 2', 'Feature 3', 'Feature 4','Feature 5 (meaningless but please still use it)', 'Feature 6', 'Feature 7', 'Feature 8', 'Feature 9', 'Feature 10']
 
-file1 = {
+sum_no_noise_file_info = {
             "name": "newsum.csv",
             "sep":";",
             "regression_target": "Target",
@@ -16,7 +15,7 @@ file1 = {
             "needs_transformation": False,
             "too_many_features": False
         }
-file2 = {
+sum_with_noise_file_info = {
             "name": "sum_ds_wn.csv",
             "sep":";",
             "regression_target": "Noisy Target",
@@ -25,7 +24,7 @@ file2 = {
             "needs_transformation": False,
             "too_many_features": False
         }
-file3 = {
+big_housing_file_info = {
             "name": "housing_dataset.csv",
             "sep":",",
             "regression_target": "SalePrice",
@@ -35,7 +34,7 @@ file3 = {
             "too_many_features": True,
             "drop_features": ["MoSold", "YrSold", "MiscFeature"]
         }
-file4 = {
+kchouse_file_info = {
             "name": "kc_house_data.csv",
             "sep":",",
             "regression_target": "price",
@@ -45,19 +44,28 @@ file4 = {
             "needs_transformation": True,
             "too_many_features": False
         }
-file5 = {
+wine_file_info = {
             "name": "winequality-red.csv",
             "sep":";",
             "regression_target": "quality",
             "classification_target":"quality",
             "features": ["fixed acidity","volatile acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol"],
-            "transform_function" : lambda x: x,
             "needs_transformation": False,
+            "too_many_features": False
+        }
+taxi_file_info = {
+            "name": "taxi.csv",
+            "sep":",",
+            "regression_target": "trip_duration",
+            "classification_target":"trip_duration",
+            "features": ["vendor_id","pickup_datetime","dropoff_datetime","passenger_count","pickup_longitude","pickup_latitude","dropoff_longitude","dropoff_latitude"],
+            "transform_function" : lambda x: round(x/60),
+            "needs_transformation": True,
             "too_many_features": False
         }
 
 
-files =  [file1,file2,file3,file4,file5]
+files =  [sum_with_noise_file_info]
 
 regression_models = ["Linear Regression", "Ridge Regression"]
 classification_models = ["Logistic Regression", "K Neighbours"]
